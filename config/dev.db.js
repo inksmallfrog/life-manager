78d2b394a501ceb2ae88f1acb5fb38cb9c2ee964
inksmallfrog <inksmallfrog@gmail.com>
2017-05-07 19:16:54 +0800

    database

--- config/dev.db.js
+++ config/dev.db.js
@@ -2,20 +2,21 @@
 * @Author: inksmallfrog
 * @Date:   2017-05-07 15:39:16
 * @Last Modified by:   inksmallfrog
-* @Last Modified time: 2017-05-07 15:42:55
+* @Last Modified time: 2017-05-07 17:18:35
 */
 
 'use strict';
 const path = require('path');
 module.exports = {
   name: 'lifeManager',
-  modelPath: join(__dirname, '../models'),
+  modelPath: path.join(__dirname, '../models'),
   db: 'lifeManager',
   username: 'youruser',
   password: 'yourpassword',
   dialect: 'mysql',
   host: '127.0.0.1',
   port: 3306,
+  logging: console.log,
   pool: {
     maxConnections: 10,
     minConnections: 0,
